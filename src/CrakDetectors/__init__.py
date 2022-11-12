83f69db19f0a32ef88e9faf3b495849b1b68bc7f
Abd-ul-Ahad <abd-ul-ahad@outlook.com>
2022-11-11 21:09:27 -0800

    Face mesh and fixes

--- src/CrakDetectors/__init__.py
+++ src/CrakDetectors/__init__.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class HandDetector:
     def __init__(self, static_image_mode_=False, maximum_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
-        
+
         self.mode = static_image_mode_
         self.maximum_num_hands = maximum_num_hands
         self.min_detection_confidence = min_detection_confidence
@@ -14,8 +14,6 @@ class HandDetector:
         self.hand_file = mp.solutions.hands
         self.hand_class = self.hand_file.Hands()
         self._draw_ = mp.solutions.drawing_utils
-
-
 
     def find_hands(self, image, draw_landmarks=True):
         """
@@ -51,10 +49,9 @@ class HandDetector:
                     for id, landmark in enumerate(hand_landmarks.landmark):
                         h, w, z = image.shape
                         cx, cy = int(h*landmark.x), int(w*landmark.y)
-                        result.append([id, cx, cy, z])
+                        result.append([id, cx, cy])
 
-        return result
-
+        return image, result
 
 
 class PoseDetector:
@@ -93,20 +90,9 @@ class PoseDetector:
 
         return image
 
-    def get_pose_landmarks(self, image, draw_landmarks=True, fill_point={
-        "id": 0,
-        "color": (255, 0, 255),
-        "radius": 10,
-        "thickness": cv2.FILLED
-     }):
+    def get_pose_landmarks(self, image, draw_landmarks=True):
         """
         Image: cv2 BGR Image,
-        fill_point: {
-            id": int,
-            "color": tuple of rgb color code,
-            "radius": int,
-            "thickness": int
-        }
         return: return array of landmarks consist of id x-axis, y-axis, z-axis
         """
         result = []
@@ -119,28 +105,22 @@ class PoseDetector:
                 # for pose_landmark in processed.pose_landmarks:
                 self._draw_.draw_landmarks(
                     image, processed.pose_landmarks, self.pose_file.POSE_CONNECTIONS)
-                for id, landmark in enumerate(processed.pose_landmarks.landmark):
+                for landmark in processed.pose_landmarks.landmark:
                     h, w, z = image.shape
                     cx, cy = int(w*landmark.x), int(h*landmark.y)
-                    result.append([id, cx, cy, z])
-                    if id == fill_point["id"]:
-                        cv2.circle(image, (cx, cy), fill_point["radius"], fill_point["color"], fill_point["thickness"])
+                    result.append([cx, cy])
 
-
-        return result
-
-
+        return image, result
 
 
 class FaceDetector:
-    def __init__(self, min_face_detection_conf = 0.5) -> None:
+    def __init__(self, min_face_detection_conf=0.5) -> None:
         self.min_detect_conf = min_face_detection_conf
 
         self.face_file = mp.solutions.face_detection
         self.Face_class = self.face_file.FaceDetection(self.min_detect_conf)
         self._draw_ = mp.solutions.drawing_utils
 
-    
     def find_face(self, image, draw_landmarks=True):
         """
         Image: cv2 BGR Image,
@@ -150,32 +130,31 @@ class FaceDetector:
         processed = self.Face_class.process(imgRGB)
 
         if draw_landmarks is True:
-             if processed.detections:
-                for id, lms in enumerate(processed.detections):
+            if processed.detections:
+                for lms in processed.detections:
                     self._draw_.draw_detection(image, lms)
 
                     lm = lms.location_data.relative_bounding_box
                     h, w, c = image.shape
 
-
-                    box = int(w*lm.xmin), int(h*lm.ymin), int(w*lm.width), int(h*lm.height)
+                    box = int(w*lm.xmin), int(h*lm.ymin), int(w *
+                                                              lm.width), int(h*lm.height)
 
                     cv2.rectangle(image, box, (255, 0, 255), 2)
-                    cv2.putText(image, f"{int(lms.score[0]*100)}%", (box[0], box[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+                    cv2.putText(
+                        image, f"{int(lms.score[0]*100)}%", (box[0], box[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
 
         return image
-
 
     def get_face_landmarks(self, image, draw_landmarks=True):
         """
         Image: cv2 BGR Image,
         return: return array of landmarks consist of id x-axis, y-axis, z-axis
         """
-        result = []
+        faces = []
 
         imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         processed = self.Face_class.process(imgRGB)
-
 
         if processed.detections:
             for id, lms in enumerate(processed.detections):
@@ -183,20 +162,47 @@ class FaceDetector:
                 lm = lms.location_data.relative_bounding_box
                 h, w, c = image.shape
 
-                box = int(w*lm.xmin), int(h*lm.ymin), int(w*lm.width), int(h*lm.height)
+                box = int(w*lm.xmin), int(h*lm.ymin), int(w *
+                                                          lm.width), int(h*lm.height)
 
                 if draw_landmarks:
                     self._draw_.draw_detection(image, lms)
                     cv2.rectangle(image, box, (255, 0, 255), 2)
-                    cv2.putText(image, f"{int(lms.score[0]*100)}%", (box[0], box[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
-            
-            
-            # print(lms)
-            result.append({
-                "id": id,
-                "image": image,
-                "bounding_box": box,
-                "land_mark": lms
-            })
+                    cv2.putText(
+                        image, f"{int(lms.score[0]*100)}%", (box[0], box[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
 
-        return result
+            # print(lms)
+                faces.append([id, box, lms.score])
+
+        return faces
+
+
+class FaceMesh:
+    def __init__(self, static_image_mode=False, max_nums_faces=1, min_detection_confidence=0.5, min_tracking_confidence=0.5) -> None:
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpFaceMesh = mp.solutions.face_mesh
+        self.FaceMesh = self.mpFaceMesh.FaceMesh(
+            static_image_mode, max_nums_faces, min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
+
+    def get_face_mesh_landmarks(self, image, draw_landmarks=True):
+        faces = []
+
+        rgbFrame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        processed = self.FaceMesh.process(rgbFrame)
+
+        if processed.multi_face_landmarks:
+            for faceLandmarks in processed.multi_face_landmarks:
+                if draw_landmarks is True:
+                    self.mpDraw.draw_landmarks(
+                        image, faceLandmarks, self.mpFaceMesh.FACEMESH_TESSELATION)
+
+                face = []
+                for landmark in faceLandmarks.landmark:
+                    h, w, z = image.shape
+                    cx, cy = int(h*landmark.x), int(w*landmark.y)
+                    face.append([cx, cy])
+                faces.append(face)
+
+        return image, faces
